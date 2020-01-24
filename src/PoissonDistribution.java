99f1153b99ae0b0ba9e947d712d38b64c2f05a52
Zahin Mohammad <zahin.dev@gmail.com>
2020-01-24 15:00:08 -0500

    test file

--- src/PoissonDistribution.java
+++ src/PoissonDistribution.java
@@ -2,13 +2,14 @@ import java.util.Random;
 
 class PoissonDistribution {
     private int lambda;
+    private Random r;
     PoissonDistribution(int lambda) {
         this.lambda = lambda;
+        // seeded with time
+        this.r = new Random();
     }
 
     double generateTimeInterval() {
-        Random r = new Random();
-
         double U = r.nextDouble();
         return (-1.0/lambda) * Math.log(1.0-U);
     }
