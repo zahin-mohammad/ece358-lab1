31f7b8ab6d1eebb9e3eaf5fb17c683a1847af111
Zahin Mohammad <zahin.dev@gmail.com>
2020-01-27 08:43:07 -0500

    replace junit with main method

--- src/SimulationTest.java
+++ src/SimulationTest.java
@@ -1,11 +1,8 @@
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class SimulationTest {
 
-    @Test
-    void runSimulationVaryTimeInfiniteBuffer() {
+    public static void main(String[] args) {
+        System.out.println("Simulation: Vary Time Infinite Buffer");
         int simulationTimeStep = 1000;
         for (int i=1; i<11; i++){
             System.out.printf("Test %d with time %d\n", i, simulationTimeStep*i);
@@ -14,11 +11,9 @@ class SimulationTest {
                     2000, 1000000, simulationTimeStep*i, 0.25);
             simulation.runSimulation();
         }
-    }
 
-    @Test
-    void runSimulationVaryTimeFiniteBuffer() {
-        int simulationTimeStep = 1000;
+        System.out.println("Simulation: Vary Time Finite Buffer");
+        simulationTimeStep = 1000;
         for (int i=1; i<11; i++){
             System.out.printf("Test %d with time %d\n", i, simulationTimeStep*i);
 
@@ -26,20 +21,24 @@ class SimulationTest {
                     2000, 1000000, simulationTimeStep*i, 4, 10);
             simulation.runSimulation();
         }
-    }
 
-    @Test
-    void runSimulationVaryQueueUtilizationInfiniteBuffer() {
+        System.out.println("Simulation: Vary Queue Utilization Infinite Buffer");
         for (double i = 0.25; i <= 0.95; i += 0.1){
             System.out.printf("Test with roh %f\n", i);
             Simulation simulation = new Simulation(
                     2000, 1000000, 1000, i);
             simulation.runSimulation();
         }
-    }
 
-    @Test
-    void runSimulationQueueUtilizationConstantInfiniteBuffer() {
+        System.out.println("Simulation: Vary Queue Utilization Infinite Buffer");
+        for (double i = 0.25; i <= 0.95; i += 0.1){
+            System.out.printf("Test with roh %f\n", i);
+            Simulation simulation = new Simulation(
+                    2000, 1000000, 1000, i);
+            simulation.runSimulation();
+        }
+
+        System.out.println("Simulation: 1.2 Queue Utilization Infinite Buffer");
         Simulation simulation = new Simulation(
                 2000, 1000000, 1000, 1.2);
         simulation.runSimulation();
