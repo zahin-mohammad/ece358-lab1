412e2b326f78763f068a4e0ad95d71a40293bcab
Zahin Mohammad <zahin.dev@gmail.com>
2020-01-27 17:23:53 -0500

    refactor random variable test

--- src/SimulationTest.java
+++ src/SimulationTest.java
@@ -25,43 +25,6 @@ class SimulationTest {
         if (argsMap.contains("q4") || argsMap.contains("Q4") || args.length <=1){
             q4();
         }
-//        System.out.println("Simulation: Vary Time Infinite Buffer");
-//        int simulationTimeStep = 1000;
-//        ArrayList<SimulationResponse> simulationResponseList = new ArrayList<>();
-//        for (int i=1; i<11; i++){
-//            System.out.printf("Test %d with time %d\n", i, simulationTimeStep*i);
-//
-//            Simulation simulation = new Simulation(
-//                    2000, 1000000, simulationTimeStep*i, 0.25);
-//            SimulationResponse simulationResponse = simulation.runSimulation();
-//            simulationResponseList.add(simulationResponse);
-//            printSimulationResponse(simulationResponse);
-//        }
-//
-//        try {
-//            createCSV(simulationResponseList, "./vary-time-infinite-buffer.csv");
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        System.out.println("Simulation: Vary Time Finite Buffer");
-//        simulationTimeStep = 1000;
-//        for (int i=1; i<11; i++){
-//            System.out.printf("Test %d with time %d\n", i, simulationTimeStep*i);
-//
-//            Simulation simulation = new Simulation(
-//                    2000, 1000000, simulationTimeStep*i, 4, 10);
-//            SimulationResponse simulationResponse = simulation.runSimulation();
-//            printSimulationResponse(simulationResponse);
-//        }
-//
-//
-//
-//        System.out.println("Simulation: 1.2 Queue Utilization Infinite Buffer");
-//        Simulation simulation = new Simulation(
-//                2000, 1000000, 1000, 1.2);
-//        SimulationResponse simulationResponse  = simulation.runSimulation();
-//        printSimulationResponse(simulationResponse);
     }
 
     private static void q1(){
@@ -73,12 +36,17 @@ class SimulationTest {
         for (int i =0; i<loop; i++){
             data.add(distribution.generateTimeInterval());
         }
-        // TODO: Print in a textfile
-        System.out.printf("q1 results:\n");
-        System.out.printf("\tlambda: %d", lambda);
-        System.out.printf("\titerations: %d", loop);
-        System.out.printf("\tmean: %f", getMean(data));
-        System.out.printf("\tvariance: %f", getVariance(data));
+        ArrayList<String> rows = new ArrayList<>();
+        double mean = getMean(data);
+        double variance = getVariance(data);
+
+        rows.add("lambda, Iterations,Mean,1/Mean,Variance,sqrt(1/Variance)");
+        rows.add(String.format("%d,%d,%f,%f,%f,%f", lambda, loop, mean, 1.0/mean, variance, Math.sqrt(1.0/variance)));
+        try {
+            createCSV(rows, "./output/q1.csv");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private static void q3(){
@@ -119,23 +87,11 @@ class SimulationTest {
         }
     }
 
-
-    private static void printSimulationResponse(SimulationResponse simulationResponse){
-        System.out.printf("Pidle, the proportion of the time that the buffer is empty: %f\n", simulationResponse.pIdle);
-        System.out.printf("Ploss, the probability of a packet being dropped (only relevant for finite buffers): %f\n",
-        simulationResponse.pLoss);
-        System.out.printf("E[N], the average number of packets in the buffer during the simulation: %f\n",
-        simulationResponse.avgPacketsInBuffer);
-    }
-
-    private static void createCSV(List<SimulationResponse> simulationResponseList, String csvName) throws IOException {
+    private static void createCSV(List<String> rows, String csvName) throws IOException {
 
         PrintWriter printWriter = new PrintWriter(new FileWriter(csvName));
-        printWriter.printf("E[N],pIdel,pLoss\n");
-
-        for (SimulationResponse simulationResponse: simulationResponseList){
-            printWriter.printf("%f,%f,%f\n",
-                    simulationResponse.avgPacketsInBuffer, simulationResponse.pIdle, simulationResponse.pLoss);
+        for (String row: rows){
+            printWriter.println(row);
         }
         printWriter.close();
     }
